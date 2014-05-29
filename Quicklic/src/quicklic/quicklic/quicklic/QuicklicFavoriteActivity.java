87da9ed2285804c68fb62ed25dd1b56cf50909d6
THYang <qksdydlsn@naver.com>
2014-05-29 16:00:26 +0900

    [THYang] Issue : Add bin folder

--- Quicklic/src/quicklic/quicklic/quicklic/QuicklicFavoriteActivity.java
+++ Quicklic/src/quicklic/quicklic/quicklic/QuicklicFavoriteActivity.java
@@ -3,14 +3,10 @@ package quicklic.quicklic.quicklic;
 import java.util.ArrayList;
 
 import quicklic.floating.api.R;
-import quicklic.quicklic.quicklic.favorite.ApkListActivity;
-import quicklic.quicklic.quicklic.favorite.PreferencesManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -18,7 +14,7 @@ public class QuicklicFavoriteActivity extends QuicklicActivity {
 
 	private ArrayList<Drawable> imageArrayList;
 	private ArrayList<String> pkgArrayList;
-	private PreferencesManager preferencesManager;
+	//	private PreferencesManager preferencesManager;
 	private PackageManager packageManager;
 
 	@Override
@@ -59,28 +55,28 @@ public class QuicklicFavoriteActivity extends QuicklicActivity {
 
 	private void getPreference()
 	{
-		preferencesManager = new PreferencesManager();
-
-		packageManager = getPackageManager();
-
-		for ( int i = 0; i < preferencesManager.getNumPreferences(this) + 1 && i < 10; i++ )
-		{
-			pkgArrayList.add(preferencesManager.getAppPreferences(this, i));
-
-			Log.e("", "" + pkgArrayList.get(0));
-
-			try
-			{
-				Drawable appIcon = packageManager.getApplicationIcon(pkgArrayList.get(i));
-				imageArrayList.add(appIcon);
-			}
-			catch (NameNotFoundException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
+		//		preferencesManager = new PreferencesManager();
+		//
+		//		packageManager = getPackageManager();
+		//
+		//		for ( int i = 0; i < preferencesManager.getNumPreferences(this) + 1 && i < 10; i++ )
+		//		{
+		//			pkgArrayList.add(preferencesManager.getAppPreferences(this, i));
+		//
+		//			Log.e("", "" + pkgArrayList.get(0));
+		//
+		//			try
+		//			{
+		//				Drawable appIcon = packageManager.getApplicationIcon(pkgArrayList.get(i));
+		//				imageArrayList.add(appIcon);
+		//			}
+		//			catch (NameNotFoundException e)
+		//			{
+		//				// TODO Auto-generated catch block
+		//				e.printStackTrace();
+		//			}
+		//
+		//		}
 
 	}
 
@@ -95,13 +91,13 @@ public class QuicklicFavoriteActivity extends QuicklicActivity {
 			if ( v.getId() == 0 )
 			{
 				System.out.println("[Hardware] " + v.getId());
-				intent = new Intent(QuicklicFavoriteActivity.this, ApkListActivity.class);
-				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//				intent = new Intent(QuicklicFavoriteActivity.this, ApkListActivity.class);
+//				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				startActivity(intent);
 			}
 			else
 			{
-				Intent intent = packageManager.getLaunchIntentForPackage(preferencesManager.getAppPreferences(getApplicationContext(), v.getId()));
+//				Intent intent = packageManager.getLaunchIntentForPackage(preferencesManager.getAppPreferences(getApplicationContext(), v.getId()));
 				startActivity(intent);
 			}
 		}
