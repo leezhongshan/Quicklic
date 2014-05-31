3393b630b7167ccbe392339b5ea2bb63e98b9fe8
THYang <qksdydlsn@naver.com>
2014-05-31 13:36:30 +0900

    [THYang] Issue : Neglect

--- Quicklic/src/quicklic/quicklic/quicklic/QuicklicActivity.java
+++ Quicklic/src/quicklic/quicklic/quicklic/QuicklicActivity.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup.LayoutParams;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
