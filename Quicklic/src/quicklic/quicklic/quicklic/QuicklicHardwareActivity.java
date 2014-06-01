6fd700be3f50fc1cfbd188a16716ea6123ff31b6
SBKim <8812ksb@naver.com>
2014-06-01 22:30:41 +0900

    [SBKim] issue : hardware -> wifi on/off기능 구현 contents : 0.onWifi(),offWifi() 메소드 구현 		   1.onClick()내에서 wifi 상태에 따라 if문으로 구분해서 onWifi()와 		     offWifi() 호출하여 Wifi 컨트롤

--- Quicklic/src/quicklic/quicklic/quicklic/QuicklicHardwareActivity.java
+++ Quicklic/src/quicklic/quicklic/quicklic/QuicklicHardwareActivity.java
@@ -3,7 +3,10 @@ package quicklic.quicklic.quicklic;
 import java.util.ArrayList;
 
 import quicklic.floating.api.R;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,6 +14,7 @@ import android.view.View.OnClickListener;
 public class QuicklicHardwareActivity extends QuicklicActivity {
 
 	private ArrayList<Drawable> imageArrayList;
+	WifiManager wifi;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState )
@@ -20,16 +24,35 @@ public class QuicklicHardwareActivity extends QuicklicActivity {
 
 		initialize();
 	}
+	
+	private void onWifi()
+	{
+		wifi.setWifiEnabled(true);
+	}
+	private void offWifi()
+	{
+		wifi.setWifiEnabled(false);
+	}
 
 	private void initialize()
 	{
 		imageArrayList = new ArrayList<Drawable>();
+		wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
 
 		// TODO
 		imageArrayList.add(getResources().getDrawable(R.drawable.hardware_test));
+		imageArrayList.add(getResources().getDrawable(R.drawable.volume_test));
+		imageArrayList.add(getResources().getDrawable(R.drawable.poweroff_test));
 
 		addViewsForBalance(imageArrayList.size(), imageArrayList, onClickListener);
 	}
+
+	/*protected void onResume()
+	{
+		initialize();
+		
+		super.onResume();
+	}*/
 
 	private OnClickListener onClickListener = new OnClickListener()
 	{
@@ -37,6 +60,26 @@ public class QuicklicHardwareActivity extends QuicklicActivity {
 		public void onClick( View v )
 		{
 			//TODO
+			if ( v.getId() == 1 )
+			{
+				startActivity(new Intent(android.provider.Settings.ACTION_SOUND_SETTINGS));
+			}
+			if ( v.getId() == 2 )
+			{
+				if(!wifi.isWifiEnabled())
+				{
+					if(wifi.getWifiState() != WifiManager.WIFI_STATE_ENABLED)
+					{
+						onWifi();
+						System.out.println("on");
+					}
+				}
+				else
+				{
+					offWifi();
+					System.out.println("off");
+				}
+			}
 		}
 	};
 
