8bd09e55d9be1ddf2389c46ed8468f1a1c54eb74
THYang <qksdydlsn@naver.com>
2014-06-26 05:42:25 +0900

    neg

--- Quicklic/src/quicklic/quicklic/favorite/QuicklicFavoriteActivity.java
+++ Quicklic/src/quicklic/quicklic/favorite/QuicklicFavoriteActivity.java
@@ -12,7 +12,6 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
